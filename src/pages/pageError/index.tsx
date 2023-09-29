25cf06353e719673b86419590de2aab61de36d35
willian de jesus pereira <williansouza9687a@gmail.com>
2023-09-29 11:35:29 -0300

    insert file vercel.json

--- src/pages/pageError/index.tsx
+++ src/pages/pageError/index.tsx
@@ -3,12 +3,13 @@ export function PageError() {
       <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
       <h1 className="text-6xl font-bold text-gray-900">404</h1>
       <h2 className="text-3xl font-bold text-gray-900 mb-4">
-        Oops! This page could not be found.
+      Página não encontrada
+
       </h2>
       <a
         href="/"
         className="py-2 px-4 bg-blue-500 text-white font-bold rounded hover:bg-blue-700"
-        >Go to homepage</a>
+        >Voltar</a>
     </div>
     )
 }
